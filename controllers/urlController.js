a1808d4db73961cd7be949803f19781d352d88f4
andrektozzi <andrektozzi@gmail.com>
2022-08-03 09:30:03 -0300

    feat: add urls id get endpoint

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -20,3 +20,22 @@ export async function addUrl(req, res) {
         return res.sendStatus(400);
     }
 }
+
+export async function getUrl(req, res) {
+    const { id } = req.params;
+
+    try {
+        const { rows: url } = await connection.query(
+            `SELECT id, "shortUrl", "url" FROM urls WHERE id = $1;`,
+            [id]
+        );
+
+        if(url.length === 0) {
+            return res.sendStatus(404);
+        }
+
+        return res.status(200).send(url);
+    } catch (error) {
+        return res.sendStatus(400);
+    }
+}
--- routes/urlRouter.js
+++ routes/urlRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { addUrl } from "../controllers/urlController.js";
+import { addUrl, getUrl } from "../controllers/urlController.js";
 import { validateUrl } from "../middlewares/urlMiddleware.js";
 import { tokenMiddleware } from "../middlewares/tokenMiddleware.js";
 
 const router = Router();
 
 router.post("/urls/shorten", tokenMiddleware, validateUrl, addUrl);
+router.get("/urls/:id", getUrl);
 
 export default router;
